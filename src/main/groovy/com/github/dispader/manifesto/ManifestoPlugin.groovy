6b0b3d2ab9e042bf1303423790a72aa74ed12f48
Jake Gage <jake.gage@gmail.com>
2016-06-24 08:40:14 -0500

    Remove 'diagnostics' task

--- src/main/groovy/com/github/dispader/manifesto/ManifestoPlugin.groovy
+++ src/main/groovy/com/github/dispader/manifesto/ManifestoPlugin.groovy
@@ -11,26 +11,6 @@ class ManifestoPlugin implements Plugin<Project> {
 
         project.extensions.create('manifesto', ManifestoPluginExtension)
 
-        project.task('diagnostics') << {
-            println "Specification-Title: ${project.name}"
-            println "Specification-Version: ${Version.specification}"
-            if ( project.manifesto.vendor ) {
-                println "Specification-Vendor: ${project.manifesto.vendor}"
-            }
-            println "Implementation-Title: ${project.name}"
-            if ( project.manifesto.vendor_id ) {
-                println "Implementation-Vendor-Id: ${project.manifesto.vendor_id}"
-            }
-            println "Implementation-Version: ${Version.implementation}"
-            if ( project.manifesto.vendor ) {
-                println "Implementation-Vendor: ${project.manifesto.vendor}"
-            }
-            if ( project.manifesto.url ) {
-                println "Implementation-URL: ${project.manifesto.url}"
-            }
-            println "Implementation-Timestamp: ${new Date()}"
-        }
-
         project.plugins.whenPluginAdded { plugin ->
             project.tasks.find { it instanceof Jar }.each {
                 it.manifest.with {
