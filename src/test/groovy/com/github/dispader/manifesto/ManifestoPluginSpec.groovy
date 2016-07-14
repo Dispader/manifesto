34522e9fe041da7290c1a62ed7fba67e6e704eba
Jake Gage <jake.gage@gmail.com>
2016-07-14 17:31:42 -0500

    Fix typographical errors

--- src/test/groovy/com/github/dispader/manifesto/ManifestoPluginSpec.groovy
+++ src/test/groovy/com/github/dispader/manifesto/ManifestoPluginSpec.groovy
@@ -49,7 +49,7 @@ class ManifestoPluginSpec extends Specification {
             'Implementation-Timestamp' | /.+/
     }
 
-    def '(plugins: war, manifesto) adds manifest attributes to the JAr'() {
+    def '(plugins: war, manifesto) adds manifest attributes to the WAr'() {
         given:
             project.pluginManager.apply 'war'
             project.pluginManager.apply 'com.github.dispader.manifesto'
