c3776734ab21fb4a4fba4f4e9304d1411f013f76
Jake Gage <jake.gage@gmail.com>
2016-09-12 22:50:32 -0500

    Update error message for unversioned projects

--- src/main/groovy/com/github/dispader/manifesto/Version.groovy
+++ src/main/groovy/com/github/dispader/manifesto/Version.groovy
@@ -17,10 +17,10 @@ class Version {
         try {
             description = git?.describe { }
         } catch(IllegalStateException ise) {
-            throw new ProjectConfigurationException('This project is not controlled by git.', null)
+            throw new ProjectConfigurationException('The manifesto plugin is only applicable to git versioned projects.', null)
         }
         if ( !description ) {
-            throw new ProjectConfigurationException('This project is not controlled by git.', null)
+            throw new ProjectConfigurationException('The manifesto plugin is only applicable to git versioned projects.', null)
         }
         description.startsWith('v') ? description.substring(1) : description
     }
