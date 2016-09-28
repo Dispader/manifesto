501059e29c98a1ebe6fe406cd30ab826d1ddc154
Jake Gage <jake.gage@gmail.com>
2016-09-28 10:58:45 -0500

    Fix #34 : Add CodeNarc checks

---
+++ src/check/codenarc/codenarc.groovy
@@ -0,0 +1,16 @@
+ruleset {
+    ruleset('rulesets/basic.xml') {
+        exclude 'EmptyCatchBlock'
+    }
+    ruleset('rulesets/braces.xml') {
+        exclude 'IfStatementBraces'
+    }
+    ruleset('rulesets/design.xml')
+    ruleset('rulesets/dry.xml')
+    ruleset('rulesets/exceptions.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/junit.xml')
+    ruleset('rulesets/logging.xml')
+    ruleset('rulesets/unnecessary.xml')
+    ruleset('rulesets/unused.xml')
+}
