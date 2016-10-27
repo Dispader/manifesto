d3253bdb64594f2011a1d2dc393a6906113d3edf
Chris Dinger <ding0057@umn.edu>
2016-10-27 00:37:53 -0500

    Add checks for missing repo, commits, tags
    Grgit will raise exceptions in the `Grgit.open().describe()` chain if:
    - no repo is present
    - no commits are present
    - no tags are present
    
    This commits adds checks for each of these so they can be handled
    gracefully. This also makes warning messages available to gradle for
    each case (which might be overkill).

--- src/test/groovy/com/github/dispader/manifesto/VersionSpec.groovy
+++ src/test/groovy/com/github/dispader/manifesto/VersionSpec.groovy
@@ -4,12 +4,34 @@ import spock.lang.*
 
 class VersionSpec extends Specification {
 
-    def 'can determine if project is controlled by Git'() {
+    def 'handles no Git repository'() {
         when:
-            Version.metaClass.static.getGit = { null }
+            Version.metaClass.static.getHasRepo = { false }
         then:
-            Version.version == '0.0.0'
+            Version.warningText == Version.MSG_NO_REPO
             Version.versioned == false
+            Version.version == null
+    }
+
+    def 'handles no Git commits'() {
+        when:
+            Version.metaClass.static.getHasRepo = { true }
+            Version.metaClass.static.getHasCommits = { false }
+        then:
+            Version.warningText == Version.MSG_NO_COMMITS
+            Version.versioned == false
+            Version.version == null
+    }
+
+    def 'handles no Git tags'() {
+        when:
+            Version.metaClass.static.getHasRepo = { true }
+            Version.metaClass.static.getHasCommits = { true }
+            Version.metaClass.static.getHasTags = { false }
+        then:
+            Version.warningText == Version.MSG_NO_TAGS
+            Version.versioned == false
+            Version.version == null
     }
 
 }
