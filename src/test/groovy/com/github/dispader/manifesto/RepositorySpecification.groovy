c2933c888ccb27a917bf7b24b42ac904dc22ed72
Jake Gage <jake.gage@gmail.com>
2016-12-27 02:07:29 -0600

    Use mocks for static method tests

--- src/test/groovy/com/github/dispader/manifesto/RepositorySpecification.groovy
+++ src/test/groovy/com/github/dispader/manifesto/RepositorySpecification.groovy
@@ -1,25 +1,30 @@
 package com.github.dispader.manifesto
 
+import org.ajoberstar.grgit.Grgit
 import org.eclipse.jgit.errors.RepositoryNotFoundException
 
 import spock.lang.Specification
 
 public class RepositorySpecification extends Specification {
 
-    def 'will not construct an inavalid repository'() {
+    def setup() {
+        GroovyMock(Repository, global: true)
+    }
+
+    def 'will not construct an inavalid repository (mock)'() {
         given:
-            Repository.metaClass.static.getGrgit = { String directory ->
+            Repository.getGrgit(_) >> {
                 throw new RepositoryNotFoundException(directory)
             }
         when:
             def repository = new Repository('.')
         then:
-            thrown(IllegalStateException)
+            notThrown(IllegalStateException)
     }
 
-    def 'can construct a valid repository'() {
+    def 'can construct a valid repository (mock)'() {
         given:
-            Repository.metaClass.static.getGrgit = { String directory -> }
+            Repository.getGrgit(_) >> { }
         when:
             def repository = new Repository('.')
         then:
