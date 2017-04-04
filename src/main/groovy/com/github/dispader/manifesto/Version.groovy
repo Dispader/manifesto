8287c56e3e83ab95a1ec4b226ab7be113e78d63d
Jake Gage <jake.gage@gmail.com>
2017-04-03 23:36:09 -0500

    Resolve final field CodeNarc violation

--- src/main/groovy/com/github/dispader/manifesto/Version.groovy
+++ src/main/groovy/com/github/dispader/manifesto/Version.groovy
@@ -6,7 +6,7 @@ class Version {
     final static String MSG_NO_COMMITS = 'The Manifesto plugin couldn\'t find any git commits.'
     final static String MSG_NO_TAGS = 'The Manifesto plugin couldn\'t find any version tags. You can create your first tag with `git tag -a v0.1.0`.'
 
-    private static Repository repository = new Repository('.')
+    private final static Repository repository = new Repository('.')
 
     private static getGit() { this.repository.grgitRepository }
 
